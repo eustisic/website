fc135f7e10d563d0d4df732352ef294130ee1bf2
Ian Eustis <eustisic@gmail.com>
2021-06-05 12:33:23 -0700

    removed comments

--- public/js/pixi-app.js
+++ public/js/pixi-app.js
@@ -1,39 +1,30 @@
 let app, displacementFilter, displacementSprite;
-let iterator = 0;
-let direction = 1;
 
-// function initPixi() {
-//   app = new PIXI.Application({width: window.innerWidth, height: window.innerHeight});
-//   document.body.appendChild(app.view);
 
-//   let image = new PIXI.Sprite.from("images/sunset.jpg");
-//   image.height = window.innerHeight;
-//   image.width = window.innerWidth;
-//   app.stage.addChild(image);
+function initPixi() {
+  app = new PIXI.Application({width: window.innerWidth, height: window.innerHeight});
+  document.body.appendChild(app.view);
 
-//   displacementSprite = new PIXI.Sprite.from("images/clouds.png");
-//   displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
-//   displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
-//   app.stage.addChild(displacementSprite);
-//   app.stage.filters = [displacementFilter];
+  let image = new PIXI.Sprite.from("images/sunset.jpg");
+  image.height = window.innerHeight;
+  image.width = window.innerWidth;
+  app.stage.addChild(image);
 
-//   animate();
-// }
+  displacementSprite = new PIXI.Sprite.from("images/clouds.png");
+  displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+  displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+  app.stage.addChild(displacementSprite);
+  app.stage.filters = [displacementFilter];
 
-// function animate() {
-//   if (iterator % 100 > 40) {
-//     direction *= -1;
-//   }
-//   console.log(direction)
-//   displacementSprite.x += (direction * 2);
-//   displacementSprite.y += (direction * 2);
-//   iterator += 1;
-//   requestAnimationFrame(animate);
-// }
+  animate();
+}
 
-/* 
-animate in 
+function animate() {
 
-*/
+  displacementSprite.x += (direction * 2);
+  displacementSprite.y += (direction * 2);
+
+  requestAnimationFrame(animate);
+}
 
 initPixi();
